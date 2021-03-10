b72b58897c4c397326a5d759dbe032868ab19ccb
Lagrange <42912437+LagrangeH@users.noreply.github.com>
2021-03-10 18:57:48 +0300

    v0.2

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-from loguru import logger
+import sys
+import time
 
+from loguru import logger
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class ZipHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        super().on_created(event)
+
+        file_extension = event.src_path.rpartition('.')[-1]
+
+
+path = 'C:\\Users\\Lagrange\\Downloads'
+
+zip_handler = ZipHandler()
+
+observer = Observer()
+observer.schedule(zip_handler, path, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
