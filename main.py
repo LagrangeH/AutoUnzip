de131fd66f8e5deccc39b48f111c39717dc1ce74
Lagrange <42912437+LagrangeH@users.noreply.github.com>
2021-03-17 17:27:29 +0300

    v0.4

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
-import sys
+# RUN THIS FILE BY ADMINISTRATOR
+# TODO: add scan archives using VirusTotal API
+import sys, os
 import time
 
-import zipfile
+from zipfile import ZipFile
+from rarfile import RarFile
 from loguru import logger
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
 path = 'C:\\Users\\Lagrange\\Downloads\\'
+available_extensions = ('rar', 'zip')
+
+
+def archiver(archive_extension, archive_path):
+    if archive_extension == 'zip':
+        return ZipFile(archive_path)
+    elif archive_extension == 'rar':
+        return RarFile(archive_path)
+    else:
+        logger.error('Расширение добавлено в список доступных, '
+                     'но не добавлено в обработчик архивов такого расширения, '
+                     'или какая-либо непредвиденная ошибка')
 
 
 class ZipHandler(FileSystemEventHandler):
     def on_created(self, event):
         super().on_created(event)
-
-        file_extension = event.src_path.rpartition('.')[-1]
-
-        if file_extension == 'zip':
-            zfile = zipfile.ZipFile(file=event.src_path)
-            zfile.extractall(f'{path}unzipped')
-            logger.debug('unzipped')
+        archive_extension = event.src_path.rpartition('.')[-1]
+        if archive_extension in available_extensions:
+            archive = archiver(archive_extension, event.src_path)
+            archive.extractall(path=event.src_path.rpartition('.')[0])
+            archive.close()
+            os.remove(event.src_path)
+        # elif archive_extension == 'rar':
+        #     logger.debug('Unrarring file')
+        #     archive = RarFile(event.src_path)
+        #     archive.extractall(path=path)
+        #     archive.close()
 
 
 zip_handler = ZipHandler()
@@ -35,5 +54,3 @@ except KeyboardInterrupt:
     observer.stop()
 
 observer.join()
-
-asdf = input()
